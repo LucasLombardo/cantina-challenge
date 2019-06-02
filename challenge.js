ff75097f6ccb96aafb73e88a0bf94cd29076501f
LucasLombardo <lucasalombardo@gmail.com>
2019-06-02 16:23:19 -0400

    add checkForMatch function

--- challenge.js
+++ challenge.js
@@ -1,5 +1,23 @@
 const readline = require(`readline`);
+const data = require("./data.json");
 
+// Checks if a node matches the given query
+const checkForMatch = (node, query) => {
+  // check if class matches
+  if (query === node.class) return true;
+  // check if class names match
+  const isClass = query[0] === `.`;
+  const isClassMatch = node.classNames && node.classNames.includes(query.slice(1));
+  if (isClass && isClassMatch) return true;
+  // check for identifier match
+  const isId = query[0] === `#`;
+  const isIdMatch = query.slice(1) === node.identifier;
+  if (isId && isIdMatch) return true;
+  // if no matches return false
+  return false;
+};
+
+// Initialize CLI
 const cli = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -12,6 +30,6 @@ cli.on(`line`, input => {
     cli.close();
     return;
   }
-  console.log(input);
+  console.log(checkForMatch(data.subviews[0], input));
   console.log(`Enter another selector to search again:`);
 });
